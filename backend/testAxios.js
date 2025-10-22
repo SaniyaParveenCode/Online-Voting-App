4d137bbb73fb28373b815548e27a7cf3e04002f9
SaniyaParveenCode <saniyayusuf433@gmail.com>
2025-10-22 08:16:30 +0530

    Remove backend folder from repo and ignore it

--- backend/testAxios.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-
-axios.get('http://localhost:8080/')
-  .then(res => console.log('Axios response:', res.data))
-  .catch(err => console.error('Axios error:', err));
